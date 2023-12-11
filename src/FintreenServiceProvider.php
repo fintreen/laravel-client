e2b04c1c5aa018198154d0b512aeebd817fc838c
fintreen <fintreen@gmail.com>
2023-12-12 00:45:34 +0100

    routes fix

--- src/FintreenServiceProvider.php
+++ src/FintreenServiceProvider.php
@@ -30,7 +30,7 @@ class FintreenServiceProvider extends ServiceProvider
 
     public function loadRoutes() {
         // Load routes
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
 
     public function loadConfigs()
