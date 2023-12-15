7e0bd1d7c30ab14550d6a1ee1dec9c5b18d8345d
fintreen <fintreen@gmail.com>
2023-12-15 16:04:31 +0100

    fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([ 'as' => 'fintreen.', 'prefix' => 'fintreen'], function() {
     Route::post('/calculate', [\Fintreen\Laravel\app\Http\Controllers\FintreenController::class,'calculateAction' ])->name('calculate');
-    Route::post('/fintreen/webhook', [\Fintreen\Laravel\app\Http\Controllers\FintreenController::class,'webHookAction' ])->name('webhook');
+    Route::post('/webhook', [\Fintreen\Laravel\app\Http\Controllers\FintreenController::class,'webHookAction' ])->name('webhook');
 });
